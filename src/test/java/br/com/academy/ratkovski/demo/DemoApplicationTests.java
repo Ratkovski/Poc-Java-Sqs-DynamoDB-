655a1cc1dea95dd73b3a260e8f982b120fec84f6
Ratkovski <drezcry@hotmail.com>
2021-10-09 21:22:42 -0300

    Setup Projeto

---
+++ src/test/java/br/com/academy/ratkovski/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.academy.ratkovski.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
